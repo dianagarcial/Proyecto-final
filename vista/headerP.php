28ff074fe812966aa249242f67573bc5afcaaefd
dianagarcial <diana_ma.garcia@uao.edu.co>
2021-11-02 09:11:43 -0500

    FALTA VERIFICAR CONSULTA INDEX

--- vista/headerP.php
+++ vista/headerP.php
@@ -9,6 +9,17 @@
 
     <body>
     
+    <div id="imghead">
+        <label for="abrir-cerrar">   
+                <img id="menu" alt="Imagen no disponible" src="imagenes/menu.png" /> </label>
+                <a href="#"><img id="logo" alt="Imagen no disponible" src="imagenes/Logos.png" /></a>
+                <h3 id="hola" class="hola">Hola, Jose Luis</h3>
+                <a href="#"><img id="notif" alt="Imagen no disponible" src="imagenes/notifications.png" /></a>
+                <a href="#"><img id="perfil" alt="Imagen no disponible" src="imagenes/perfilprofe.png" /></a>
+            </div>
+
+    <body>
+    
                
         <div class="conte">
             
@@ -21,34 +32,29 @@
                 <ul class="menu" id="menus">
                     
                     <li><a href="index.html">Inicio</a></li>
-                    <li><a href="#">Periodo académico</a></li>
-                    <li><a href="#">Asignaturas</a></li>
+                    <li><a href="#">Mis Actividades</a></li>
+    
                     <li class="submenuSO">
                         <a href="#"><span class="icon-rocket"></span>Student Outcomes(SO)<span class="caret icon-arrow-down6"></span></a>
-                        <ul class="children">
-                            <li><a href="so1.html">SO1<span class="icon-dot"></span></a></li>
-                            <li><a href="so2.html">SO2 <span class="icon-dot"></span></a></li>
-                            <li><a href="so3.html">SO3 <span class="icon-dot"></span></a></li>
-                            <li><a href="#">SO4 <span class="icon-dot"></span></a></li>
-                            <li><a href="#">SO5 <span class="icon-dot"></span></a></li>
-                            <li><a href="#">SO6 <span class="icon-dot"></span></a></li>
-                            <li><a href="#">SO7 <span class="icon-dot"></span></a></li>
-                            
-                        </ul>
+                    
                     </li>
-            
-            
+                     <li><a href="#">Asignaturas</a></li>
+                    <ul class="children">
+                        <li><a href="#">Grupos<span class="icon-dot"></span></a></li>
+                       
+                    </ul>
+    
+    
                     <li class="submenuSO">
                         <a href="#"><span class="icon-rocket"></span>Gestión documental<span class="caret icon-arrow-down6"></span></a>
                         <ul class="children">
-                            <li><a href="#">Recibidos<span class="icon-dot"></span></a></li>
-                            <li><a href="#">Pendientes<span class="icon-dot"></span></a></li>
-                            <li><a href="#">Aprobados<span class="icon-dot"></span></a></li>
-                            <li><a href="#">Rechazados<span class="icon-dot"></span></a></li>
-                           
+                            <li><a href="#">Aprobado<span class="icon-dot"></span></a></li>
+                            <li><a href="#">Por aprobación<span class="icon-dot"></span></a></li>
+                            <li><a href="#">Rechazada<span class="icon-dot"></span></a></li>
+                            <li><a href="#">Sin enviar<span class="icon-dot"></span></a></li>
+    
                         </ul>
-
-                        <li><a href="ini.php">Registro Actividades</a></li>
+                   
+          
                 </ul>
                 </div>
-            
