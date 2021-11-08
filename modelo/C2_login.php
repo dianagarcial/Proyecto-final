904246245b05c06b8382d983534cd3834b8baadb
davidsc1509 <david.sanchez_col@uao.edu.co>
2021-11-08 00:26:33 -0500

    Login_2 "Funcionando"
    Login funcionando exepto por la parte de los usuarios en especifico y el control de acceso por medio de la url

---
+++ modelo/C2_login.php
@@ -0,0 +1,63 @@
+<?php
+$usuario = htmlentities($_POST['usuario']);
+$contraseña = htmlentities($_POST['clave']);
+
+
+$cifrada = md5($contraseña);
+
+session_start();
+$_SESSION['usuario']=$usuario;
+
+require_once('conexion.php');
+$link=Db::conectar();
+//$consulta = "SELECT * FROM usuario WHERE nomUsuario='$usuario' AND contrasena = '$contraseña'";
+//$resultado = mysql_query($consulta, $link);
+
+$consulta = $link-> query("SELECT * FROM usuario WHERE nomUsuario='$usuario' AND contrasena = '$cifrada'");
+
+//$consulta = $link-> query("SELECT * FROM usuario WHERE nomUsuario='$usuario'");
+
+$filas = $consulta->rowcount();
+
+if ($filas){
+   
+
+
+/*     switch ($tipoU){
+        case "Director":
+            header("Location: ../vista/indexD.php");
+            break;
+        case "Profesor":
+            header("Location: ../vista/indexP.php");
+            break;
+    } */
+
+        header("Location: ../vista/indexD.php");
+    
+/*     }elseif($tipoU == "Profesor"){
+        header("Location: ../vista/indexP.php");
+    } */
+
+   /*  if ($link-> query("SELECT * FROM usuario WHERE nomUsuario='$usuario' AND tipoUsuario ='Director'")){
+        header("Location: ../vista/indexD.php");
+    }elseif($link-> query("SELECT * FROM usuario WHERE nomUsuario='$usuario' AND tipoUsuario ='Profesor'")){
+        header("Location: ../vista/indexP.php");
+    }else{
+        header("Location: ../vista/index.php");
+    } */
+
+}else {
+    ?>
+    <?php
+    include("Location: ../vista/login.php");
+    ?>
+    echo'<script type="text/javascript">
+        alert("Contraseña incorrecta");
+        window.location.href="../vista/login.php";
+        </script>';
+    <?php
+    
+}
+
+mysql_close($link);
+?>
